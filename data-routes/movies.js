93fa6f3265f11dcd6011a1d53b36c061a00e43ca
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-29 00:46:57 -0800

    correcting .post and .delete routs for movies

--- data-routes/movies.js
+++ data-routes/movies.js
@@ -31,7 +31,18 @@ router
         res.json({ message: 'Get all movies.'});   
     })
     .post((req, res) => {                               // Creating a new movie
-        res.json({ message: 'Create a new movie.'});
+        let { title, year } = req.body;                  // Extracting the title and the year from the request body (unsure if we need add a rating in this case though)
+
+        // Generating a new movie objest so it then gets added to the array of movies
+        const newMovie = {
+            id: movies.length + 1,
+            title,
+            year,
+            rating
+        };
+        movies.push(newMovie);
+
+        res.json({ message: 'Successfully created a new movie.', movie: newMovie});
     });
 
 router
@@ -40,7 +51,20 @@ router
     res.json({ message: `Update movie with ID ${req.params.id}.`});
     })
     .delete((req, res) => {                   // Deleting a movie by ID
-        res.json({ message: `Deleting the movie with ID ${req.params.id}`});
+        const movieId = parseInt(req.params.id);        // parsing the id parameter to an integer to ensure it's a valid ID
+    
+    // Find the index of the movie with the given ID
+    const index = movies.findIndex(movie => movie.id === movieId);      // searching for the movie in the movies array based on the provided ID 
+
+    // Check if the movie exists
+    if (index !== -1) {                     // if the movie is found (index is not negative)...
+        // Remove the movie from the array
+        movies.splice(index, 1);            //...use splice to remove to remove the movie from the array
+        res.json({ message: `Deleted movie with ID ${movieId}.` });
+    } else {
+        // Movie not found
+        res.status(404).json({ error: `Movie with ID ${movieId} not found.` });
+    }
     })
 
 
